0d3aeb5e4ca20105406675f29cdf9f4288eafb12
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2025-09-04 18:52:47 -0400

    add cancel button  and  add funtionallity

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTask } from '../task/task.models';
+
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  @Output() add = new EventEmitter< NewTask>();
 
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
+  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,36 +1,61 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component'; // Correct import
+import { TaskComponent } from './task/task.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { NewTask } from './task/task.models';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
-  imports: [TaskComponent],
+  imports: [TaskComponent, AddTaskComponent],
 })
-export class TasksComponent { // Correct class name
-  @Input({required: true}) id!: string;
-  @Input({required: true}) name!: string;
+export class TasksComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask = false;
+
   tasks = [
     {
       id: 't1',
       userId: 'u1',
       title: 'Master Angular',
       summary: 'Learn the basics of Angular framework and build a simple app.',
-      dueDate: '2025-12-31'
+      dueDate: '2025-12-31',
     },
     {
       id: 't2',
       userId: 'u2',
       title: 'Master Angular',
       summary: 'Learn the basics of Angular framework and build a simple app.',
-      dueDate: '2025-12-31'
-    }
+      dueDate: '2025-12-31',
+    },
   ];
+
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.id);
   }
+
   onCompleteTask(taskId: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onTaskAdded(taskData: NewTask)  {
+    this.tasks.unshift({
+      id: new Date().getDate().toString(),
+      userId: this.id,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+    this.isAddingTask = false;
   }
 }
